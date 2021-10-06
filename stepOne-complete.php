468b3a106db885f667421bacbf65ec8b560a33ad
Jesse <70843294+jessep92@users.noreply.github.com>
2021-10-06 15:33:54 -0700

    Complete step one of assignment module

---
+++ stepOne-complete.php
@@ -0,0 +1,20 @@
+<!-- 
+    Jesse Peltonen
+    10/06/21
+-->
+
+<?php
+
+// Step 1 of the assignment complete :)
+$github_fundamentals = "Learn Git & GitHub - Completed";
+echo "<h2>" . $github_fundamentals . "</h2>";
+
+?>
+
+<!-- 
+
+    I feel like I already had a pretty solid grasp on the GitHub GUI
+    and how Git worked in general. Just having fun before the next steps of the assignment.
+    However, I did not know about profile README's, that's pretty fun.
+
+-->
